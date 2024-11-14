96e9f53fbd9778be9855387ec4a19dce0d130cb0
Valya Kulakova <kulakova-vv@mail.ru>
2024-11-14 20:43:30 +0300

    func_url

--- weather (type hinting)/weather_api_service.py
+++ weather (type hinting)/weather_api_service.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 from typing import NamedTuple
 
+import urllib
+from urllib.error import URLError
+
 from get_gps import Coordinates
 from enum import Enum
-
+import ssl
+from exceptions import ApiServiceError
+from config import OPENWEATHER_URL
 Celsius = int
 
 class WeatherType(Enum):
@@ -23,5 +28,19 @@ class Weather(NamedTuple):
     city: str
 
 def get_weather(coordinates: Coordinates) -> Weather:
-    pass
+    openweather_response = _get_openweather_response(
+        longitude=coordinates.longitude, latitude=coordinates.latitude)
+    weather = _parse_openweather_response(openweather_response)
+    return weather
+
+def _get_openweather_response(longitude=float, latitude=float) -> str:
+    ssl._create_default_https_context = ssl._create_unverified_context
+    url = OPENWEATHER_URL.format(longitude=longitude, latitude=latitude)
+    try:
+        return urllib.request.urlopen(url).read()
+    except URLError:
+        raise ApiServiceError
+
+def _parse_openweather_response(openweather_response:str) -> Weather:
+
 
