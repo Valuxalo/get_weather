cd45d3d96a07acf16b3248eaa90de42c606aea22
Valya Kulakova <kulakova-vv@mail.ru>
2024-11-17 19:49:03 +0300

    add functions in wather_api_service

--- weather (type hinting)/exceptions.py
+++ weather (type hinting)/exceptions.py
@@ -1,2 +1,12 @@
 class ApiServiceError(Exception):
     """Program can't receive current weather"""
+
+# from urllib.request import urlopen
+
+ 
+# url = "https://api.openweathermap.org/data/2.5/weather?" \
+#     "lat=60.02&lon=30.42&appid=56f96e1797aba38fcb8f00b318c45d96" \
+#     "&lang=ru&units=metric"
+# response = urlopen(url)
+ 
+# print(response.read())
--- weather (type hinting)/weather_api_service.py
+++ weather (type hinting)/weather_api_service.py
@@ -1,12 +1,14 @@
 from datetime import datetime
-from typing import NamedTuple
+from typing import Literal, NamedTuple
 
-import urllib
+from urllib.request import urlopen
 from urllib.error import URLError
 
 from get_gps import Coordinates
 from enum import Enum
 import ssl
+import json
+from json import JSONDecodeError
 from exceptions import ApiServiceError
 from config import OPENWEATHER_URL
 Celsius = int
@@ -37,10 +39,49 @@ def _get_openweather_response(longitude=float, latitude=float) -> str:
     ssl._create_default_https_context = ssl._create_unverified_context
     url = OPENWEATHER_URL.format(longitude=longitude, latitude=latitude)
     try:
-        return urllib.request.urlopen(url).read()
+        return urlopen(url).read()
     except URLError:
         raise ApiServiceError
 
 def _parse_openweather_response(openweather_response:str) -> Weather:
+    try:
+        opeweather_dict = json.load(openweather_response)
+    except JSONDecodeError:
+        raise ApiServiceError
+    return Weather(
+        temperature=_parse_temperature(opeweather_dict),
+        weather_type=_parse_weather_type(opeweather_dict),
+        sunrise=_parse_sun_time(opeweather_dict, 'sunrise'),
+        sunset=_parse_sun_time(opeweather_dict, 'sunset'),
+        city=_parse_city(opeweather_dict)
+        )
+    
 
+def _parse_temperature(opeweather_dict:dict) -> Celsius:
+    return round(opeweather_dict['main']['temp'])
 
+def _parse_weather_type(opeweather_dict:dict) -> WeatherType:
+    try:
+        weather_type_id = str(opeweather_dict['weather'][0]['id'])
+    except (IndexError, KeyError):
+        raise ApiServiceError
+    weather_types = {
+        '2': WeatherType.Thunderstorm,
+        '3': WeatherType.Drizzle,
+        '5': WeatherType.Rain,
+        '6': WeatherType.Snow,
+        '7': WeatherType.Atmosphere,
+        '800': WeatherType.Clear,
+        '80': WeatherType.Clouds
+    }
+    for _id, _weather_type in weather_types.items():
+        if weather_type_id.startswith(_id):
+            return _weather_type
+
+def _parse_sun_time(
+        opeweather_dict:dict,
+        time: Literal["sunrise","sunset"]) -> datetime:
+    return datetime.fromtimestamp(opeweather_dict['sys'][time]).strftime('%Y-%m-%d %H:%M:%S')
+
+def _parse_city(opeweather_dict:dict) -> str:
+    return opeweather_dict['name']
